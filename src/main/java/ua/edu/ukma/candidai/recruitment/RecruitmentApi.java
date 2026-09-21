be0ba90dc575303680c88916ffa8521e6f6670ff
d.koriavets <d.koriavets@knubitech.com>
2026-09-22 00:12:04 +0300

    feat(modulith): expose cross-module APIs for vacancy details and recruitment status updates

---
+++ src/main/java/ua/edu/ukma/candidai/recruitment/RecruitmentApi.java
@@ -0,0 +1,12 @@
+package ua.edu.ukma.candidai.recruitment;
+
+import ua.edu.ukma.candidai.recruitment.dto.model.ApplicationStatus;
+
+import java.util.UUID;
+
+public interface RecruitmentApi {
+
+    ApplicationDetails getApplication(UUID applicationId);
+
+    void updateStatus(UUID applicationId, ApplicationStatus status, String comment);
+}
---
+++ src/main/java/ua/edu/ukma/candidai/recruitment/dto/model/package-info.java
@@ -0,0 +1,2 @@
+@org.springframework.modulith.NamedInterface("model")
+package ua.edu.ukma.candidai.recruitment.dto.model;
