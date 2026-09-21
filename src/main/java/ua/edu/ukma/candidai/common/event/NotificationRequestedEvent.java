556c0b7f918db129bd86a82e366023e47e5f1b92
humaNukr <artemgritsenko2007@gmail.com>
2026-09-21 11:43:23 +0300

    refactor(notification): remove NotificationRequestedEvent and listener
    - Deleted NotificationRequestedEvent class and its listener.
    - Cleaned up unused notification handling code to streamline the notification system.
    - Improved overall code maintainability by removing obsolete components.

--- src/main/java/ua/edu/ukma/candidai/common/event/NotificationRequestedEvent.java
+++
@@ -1,9 +0,0 @@
-package ua.edu.ukma.candidai.common.event;
-
-import java.util.UUID;
-
-public record NotificationRequestedEvent(
-        UUID recipientId,
-        String subject,
-        String body
-) {}
